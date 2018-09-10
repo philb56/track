c6f8a7a26b9cbce7ce4cfbb49de4c0e8ba235777
PhilB56 <philb56@users.noreply.github.com>
2018-09-10 20:47:34 +0100

    First commit cloned from devconnector/tree/a95f3eb1e900ea0cb49cd6500713504fff6fe62f

---
+++ README.md
@@ -0,0 +1,13 @@
+# track
+
+## Start Point
+
+The start point for this project is cloned from devconnector/tree/a95f3eb1e900ea0cb49cd6500713504fff6fe62f
+
+This includes all user registration + login logic
+
+# Config
+
+./config/keys.js should export:
+mongoURI: "mongodb://`<un>:<pw>@<db>`"
+secretOrKey: "<somesecret>"
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const profile = require("./routes/api/profile");
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//db config
+const db = require("./config/keys").mongoURI;
+
+//connect to DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+//Routes
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
